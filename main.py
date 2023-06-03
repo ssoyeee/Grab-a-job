c8a44e6f5f1b3a54f7fd515ae5940f65dc3755a2
ssoyeee <busanpsy1@gmail.com>
2023-06-03 00:35:34 -0400

    send arguents to /search

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from extractors.wwr import extract_wwr_jobs
+# from extractors.indeed import extract_indeed_jobs
+
 
 app = Flask("JobScrapper")
 
@@ -8,6 +11,10 @@ def home():
     return render_template("home.html", name="soyeon")
 
 @app.route("/search")
-def hello():
-    return render_template("search.html")
+def search():
+    keyword = request.args.get("keyword")
+    # indeed = extract_indeed_jobs(keyword)
+    wwr = extract_wwr_jobs(keyword)
+    # jobs = indeed + wwr
+    return render_template("search.html", keyword=keyword, wwr=wwr)
 app.run("127.0.0.1")
