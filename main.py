fab0593c888130e539eadb820d7feccd0ef4cdf5
ssoyeee <busanpsy1@gmail.com>
2023-06-02 23:50:50 -0400

    refactoring: save_to_file

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from extractors.indeed import extract_indeed_jobs
 from extractors.wwr import extract_wwr_jobs
+from file import save_to_file
 
 
 keyword = input("What do you want to search for? ")
@@ -13,11 +14,4 @@ wwr = extract_wwr_jobs(keyword) # return list
 
 jobs = indeed+wwr
 
-file = open(f"{keyword}.csv", "w")
-file.write("Position,Company,Location,URL\n")
-
-
-for job in jobs:
-    file.write(f"{job['position']},{job['company']},{job['location']},{job['link']}\n")
-file.close()
-
+save_to_file(keyword, jobs)
