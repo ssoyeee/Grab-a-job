d2a17e76dd4a6d8e0ecfcb12abb674065dd60d17
ssoyeee <busanpsy1@gmail.com>
2023-06-04 14:40:54 -0400

    search results

--- main.py
+++ main.py
@@ -15,6 +15,7 @@ def search():
     keyword = request.args.get("keyword")
     # indeed = extract_indeed_jobs(keyword)
     wwr = extract_wwr_jobs(keyword)
+    jobs = wwr
     # jobs = indeed + wwr
-    return render_template("search.html", keyword=keyword, wwr=wwr)
-app.run("127.0.0.1")
+    return render_template("search.html", keyword=keyword, jobs=jobs)
+app.run("127.0.0.1", port=8000, debug=True)
